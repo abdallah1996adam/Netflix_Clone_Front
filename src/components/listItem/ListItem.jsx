b365e04182079f42ed4c9ec6873a2242b743ffba
abdallah1996adam <abdallahaadam@protonmail.com>
2021-09-21 11:31:20 +0200

    homePage refactoring :recycle:

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 //SCSS
 import "./listItem.scss";
@@ -10,18 +10,44 @@ import {
   ThumbDownOutlined,
   ThumbUpAltOutlined,
 } from "@material-ui/icons";
-const ListItem = () => {
+const ListItem = ({ index }) => {
+  const [isHovred, setIsHovred] = useState(false);
+  const trailer =
+    "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   return (
-    <div className="list-item">
+    <div
+      className="list-item"
+      onMouseEnter={() => setIsHovred(true)}
+      onMouseLeave={() => setIsHovred(false)}
+      style={{ left: isHovred && index * 225 - 50 + index * 3 }}
+    >
       <img src={movie2} alt="movie pic" />
-      <div className="item-info">
-        <div className="icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpAltOutlined />
-          <ThumbDownOutlined />
-        </div>
-      </div>
+      {isHovred && (
+        <>
+          <video src={trailer} loop autoPlay={true} />
+          <div
+            className="item-info"
+            style={{ visibility: isHovred ? "visible" : "hidden" }}
+          >
+            <div className="icons">
+              <PlayArrow />
+              <Add />
+              <ThumbUpAltOutlined />
+              <ThumbDownOutlined />
+            </div>
+            <div className="item-info-top">
+              <span>1hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>1999</span>
+            </div>
+            <div className="desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum
+              ipsam eveniet delectus. Voluptatum nobis ratione quisquam.
+            </div>
+            <div className="genre">Action</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
